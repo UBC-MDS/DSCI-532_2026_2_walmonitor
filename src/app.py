52b1b1367a9eda7de23ba5852db48a092b00d93b
mailysg8 <mai.guedon@gmail.com>
2026-02-26 22:40:15 -0800

    Add option to compare different things in plot

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from datetime import date
 from shiny import App, ui, render
-import pandas as pd
 import altair as alt
-from shinywidgets import render_altair, output_widget 
+import pandas as pd
+from shinywidgets import render_altair, output_widget
 
 ## Input choices
 METRIC_CHOICES = {
@@ -22,6 +22,7 @@ BRANCH_CHOICES = {
 # Default date range
 DEFAULT_START = date(2019, 1, 1)
 DEFAULT_END = date(2019, 3, 30)
+
 
 ## App interface
 app_ui = ui.page_fluid(
@@ -95,22 +96,38 @@ app_ui = ui.page_fluid(
             ui.layout_columns(
                 ui.card(
                     ui.card_header("Sales Mix Over Time"),
-                    ui.panel_conditional(   # Used Claude to suggest which ui.* to use for adding a slider conditional on user input
-                        "input.agg === 'day'",
-                        ui.input_slider(
-                            "range",
-                            "Select a date range",
-                            min = pd.to_datetime('2019-01-01'),
-                            max = pd.to_datetime('2019-03-31'),
-                            value = [pd.to_datetime('2019-01-01'),pd.to_datetime('2019-01-31')],
-                            ticks = True,
-                            step = 1,
-                            time_format="%Y-%m-%d"
+                    ui.layout_columns(
+                        ui.panel_conditional(   # Used Claude to suggest which ui.* to use for adding a slider conditional on user input
+                            "input.agg === 'day'",
+                            ui.input_slider(
+                                "range",
+                                "Select a date range:",
+                                min = pd.to_datetime('2019-01-01'),
+                                max = pd.to_datetime('2019-03-31'),
+                                value = [pd.to_datetime('2019-02-01'),pd.to_datetime('2019-02-28')],
+                                ticks = True,
+                                step = 1,
+                                time_format="%Y-%m-%d"
+                            ),
+                              ui.help_text(
+                                  "Suggested range size : 1 month"
+                                  ),
                         ),
-                        ui.help_text("Suggested range size : 1 month"),
+                        ui.input_select(
+                            "comp",
+                            "Compare",
+                            choices={
+                                "Product line": "Product line",
+                                "Payment": "Payment type",
+                                "Gender": "Gender",
+                                "Customer type": "Customer type",
+                            },
+                            selected="product_line",
+                        ),
+                        col_widths=[7, 5]
                     ),
                     output_widget("stack_plot"),
-                    full_screen = True
+                    full_screen=True
                 ),
                 ui.card(
                     ui.card_header("Ranked Product Lines"),
@@ -154,21 +171,26 @@ def server(input, output, session):
     def stack_plot():
         walmart_df = pd.read_csv('data/raw/walmart_sales_data.csv')
         walmart_df['Date']=pd.to_datetime(walmart_df['Date'])
+        input_comparison = input.comp()
 
         # If user chooses to aggregate by day 
         if input.agg()=='day':
             start_date = pd.to_datetime(input.range()[0])
             end_date = pd.to_datetime(input.range()[1])
-            prod_sum = walmart_df[walmart_df["Date"].between(start_date, end_date, inclusive='both')].groupby([walmart_df['Date'],walmart_df['Product line']]).agg({'Total':'mean'})
-        else: # If user chooses to aggregate by week
-            prod_sum = walmart_df.groupby(['Product line', pd.Grouper(key='Date', freq='W-MON')]).agg({'Total':'mean'})
+            prod_sum = walmart_df[walmart_df["Date"].between(start_date, end_date, inclusive='both')].groupby([walmart_df['Date'],walmart_df[input_comparison]]).agg({'Total':'mean'})
         
+        # If user chooses to aggregate by week
+        else:
+            prod_sum = walmart_df.groupby([input_comparison, pd.Grouper(key='Date', freq='W-MON')]).agg({'Total':'mean'})
         chart = alt.Chart(prod_sum.reset_index()).mark_area().encode(
             y=alt.Y('Total',title='Total sales'),
             x = 'Date:T',
-            color = 'Product line',
-            tooltip=['Product line','Date','Total']
+            color = input_comparison,
+            tooltip=[input_comparison,'Date','Total']
         )
+        
         return chart
+
+    
 
 app = App(app_ui, server)
