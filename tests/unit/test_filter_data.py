31492a60548d3e78695bd73b259a12d14fbffe00
mailysg8 <mai.guedon@gmail.com>
2026-03-12 19:44:32 -0700

    Create test module and add pytest to environment

---
+++ tests/unit/test_filter_data.py
@@ -0,0 +1,7 @@
+"""
+Tests for the filter_data() function in the helper_function module.
+
+The function filter_data() is used to filter a dataframe using given inputs.
+"""
+import pandas as pd
+import pytest
